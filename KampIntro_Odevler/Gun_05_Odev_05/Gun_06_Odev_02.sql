18ac10d852fd5972f88753b177688908b3fe2119
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-03-04 20:29:36 +0300

    GÜn 6 Ödevler bitti

---
+++ KampIntro_Odevler/Gun_05_Odev_05/Gun_06_Odev_02.sql
@@ -0,0 +1,6 @@
+﻿SELECT p.ProductName [Ürün Adı], SUM(Details.Quantity*Details.UnitPrice) [Kazanılan Toplam Tutar]
+FROM Products p
+INNER JOIN [Order Details] Details ON p.ProductID = Details.ProductID 
+INNER JOIN Orders o ON o.OrderID = Details.OrderID
+GROUP BY ProductName 
+ORDER BY ProductName ASC
