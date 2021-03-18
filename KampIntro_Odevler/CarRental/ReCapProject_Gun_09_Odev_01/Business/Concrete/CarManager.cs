07a18155c3f4251309ea9e7e7a757416dcf9b26c
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-03-18 17:35:10 +0300

    9. gün 1. mola

---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_09_Odev_01/Business/Concrete/CarManager.cs
@@ -0,0 +1,47 @@
+﻿using Business.Abstract;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CarManager : ICarService
+    {
+        ICarDal _carDal;
+        public CarManager(ICarDal carDal)
+        {
+            _carDal = carDal;
+        }
+        public void Add(Car car)
+        {
+            if (car.Description.Length < 3)
+            {
+                Console.WriteLine("Araç adı en az iki karakter olmalıdır");
+            }
+            else if (car.DailyPrice < 1)
+            {
+                Console.WriteLine("Araç günlük ücreti sıfırdan büyük olmalıdır");
+            }
+            else _carDal.Add(car);
+        }
+        public List<Car> GetAll()
+        {
+            return _carDal.GetAll();
+        }
+
+        public List<Car> GetCarsByBrandId(int id)
+        {
+            return _carDal.GetAll(p => p.BrandId == id);
+        }
+
+        public List<Car> GetCarsByColorId(int id)
+        {
+            return _carDal.GetAll(p => p.ColorId == id);
+        }
+    }
+}
---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_09_Odev_01/ConsoleUI/Program.cs
@@ -0,0 +1,34 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new EfCarDal());
+
+            foreach (var car in carManager.GetCarsByBrandId(1))
+            {
+                Console.WriteLine(car.Id+"/"+
+                    car.ColorId+"/"+
+                    car.BrandId+"/"+
+                    car.DailyPrice+"/"+
+                    car.Description+"/"+
+                    car.ModelYear);
+            }
+            foreach (var car in carManager.GetCarsByColorId(1))
+            {
+                Console.WriteLine(car.Id + "/" +
+                    car.ColorId + "/" +
+                    car.BrandId + "/" +
+                    car.DailyPrice + "/" +
+                    car.Description + "/" +
+                    car.ModelYear);
+            }
+        }
+    }
+}
