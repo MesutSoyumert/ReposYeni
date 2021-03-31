11db55a401c0f98841ca2f191c8f323505e7880f
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-03-31 16:33:41 +0300

    GÜn 14 günbaşı hazırlık

---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_14_Odev_02/Business/Concrete/BrandManager.cs
@@ -0,0 +1,94 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Aspects.Validation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class BrandManager : IBrandService
+    {
+        IBrandDal _brandDal;
+        public BrandManager(IBrandDal brandDal)
+        {
+            _brandDal = brandDal;
+        }
+        
+        [ValidationAspect(typeof(BrandValidator))]
+        public IResult Add(Brand brand)
+        {
+            IResult result = BusinessRules.Run(CheckIfBrandNameExists(brand.Name));
+            if (result != null)
+            {
+                return result;
+            }
+            _brandDal.Add(brand);
+            return new SuccessResult(Messages.BrandAdded);
+        }
+
+        public IResult Delete(Brand brand)
+        {
+            IResult result = BusinessRules.Run(CheckIfBrandToDeleteCarsHasBrand(brand.Id));
+            if (result != null)
+            {
+                return result;
+            }
+            _brandDal.Delete(brand);
+            return new SuccessResult(Messages.BrandDeleted);
+        }
+
+        public IDataResult<List<Brand>> GetAll()
+        {
+            return new SuccessDataResult<List<Brand>>(_brandDal.GetAll(), Messages.BrandsListed);
+        }
+
+        public IDataResult<Brand> GetById(int id)
+        {
+            return new SuccessDataResult<Brand>(_brandDal.Get(p => p.Id == id), Messages.BrandFound);
+        }
+        
+        [ValidationAspect(typeof(BrandValidator))]
+        public IResult Update(Brand brand)
+        {
+            IResult result = BusinessRules.Run(CheckIfBrandNameExists(brand.Name));
+            if (result != null)
+            {
+                return result;
+            }
+            _brandDal.Update(brand);
+            return new SuccessResult(Messages.BrandUpdated);
+        }
+        private IResult CheckIfBrandNameExists(string Name)
+        {
+            var result = _brandDal.GetAll(p => p.Name == Name).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.BrandNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfBrandToDeleteCarsHasBrand(int id)
+        {
+            //CarManager carManager = new CarManager(new EfCarDal());
+
+            //var result = carManager.GetCarsByBrandId(id);
+
+            //if (result != null && result.Data.Count > 0)
+            //{
+            //    return new ErrorResult(Messages.BrandToDeleteCarsHasBrand);
+            //}
+            return new SuccessResult();
+        }
+    }
+}
---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_14_Odev_02/Business/Concrete/RentalManager.cs
@@ -0,0 +1,113 @@
+﻿using Business.Abstract;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Aspects.Validation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class RentalManager : IRentalService
+    {
+        IRentalDal _rentalDal;
+        ICarService _carService;
+        ICustomerService _customerService;
+        public RentalManager(IRentalDal rentalDal, ICarService carService, ICustomerService customerService)
+        {
+            _rentalDal = rentalDal;
+            _carService = carService;
+            _customerService = customerService;
+        }
+
+        [ValidationAspect(typeof(RentalValidator))]
+        public IResult Add(Rental rental)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarExists(rental.CarId),
+                                               CheckIfRentalCarIsAlreadyRented(rental.CarId),
+                                               CheckIfCustomerExists(rental.CustomerId));
+            if (result != null)
+            {
+                return result;
+            }
+            _rentalDal.Add(rental);
+            return new SuccessResult(Messages.RentalAdded);
+        }
+
+        public IResult Delete(Rental rental)
+        {
+            _rentalDal.Delete(rental);
+            return new SuccessResult(Messages.RentalDeleted);
+        }
+
+        public IDataResult<List<Rental>> GetAll()
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(), Messages.RentalsListed);
+        }
+
+        public IDataResult<Rental> GetById(int id)
+        {
+            return new SuccessDataResult<Rental>(_rentalDal.Get(p => p.Id == id), Messages.RentalFound);
+        }
+
+        public IDataResult<List<Rental>> GetRentalsByCustomerId(int id)
+        {
+            return new SuccessDataResult<List<Rental>>(_rentalDal.GetAll(p => p.CustomerId == id && p.ReturnDate == new DateTime(0001, 01, 01, 0, 0, 0)), Messages.RentalsByCutomerIdListed);
+        }
+
+        [ValidationAspect(typeof(RentalValidator))]
+        public IResult Update(Rental rental)
+        {
+            IResult result = BusinessRules.Run(CheckIfCarExists(rental.CarId),
+                                               CheckIfRentalCarIsAlreadyRented(rental.CarId),
+                                               CheckIfCustomerExists(rental.CustomerId));
+            if (result != null)
+            {
+                return result;
+            }
+            _rentalDal.Update(rental);
+            return new SuccessResult(Messages.RentalUpdated);
+        }
+        private IResult CheckIfCarExists(int CarId)
+        {
+            var result = _carService.GetById(CarId);
+
+            if (result.Success == false || result.Data == null)
+            {
+                return new ErrorResult(Messages.CarNotFound);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfRentalCarIsAlreadyRented(int CarId)
+        {
+            var result = _carService.GetById(CarId);
+
+            if (result.Success == false || result.Data == null)
+            {
+                return new ErrorResult(Messages.CarNotFound);
+            }
+            if (result.Data.IsRented == true)
+            {
+                return new ErrorResult(Messages.RentalCarIsAlreadyRented);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCustomerExists(int CustomerId)
+        {
+            var result = _customerService.GetById(CustomerId);
+
+            if (result.Success == false || result.Data == null)
+            {
+                return new ErrorResult(Messages.CarNotFound);
+            }
+            return new SuccessResult();
+        }
+    }
+}
