edd3f54bc7f2d16cf93bcaf2cb2d2f32aca9d571
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-04-03 21:24:03 +0300

    GÜn 15 Ödev 01 bitti. Cache, Transaction ve Performance aspectlerini eklendi. Sadece brandmanagere uygulandı

---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_15_Odev_01/Business/Concrete/CustomerManager.cs
@@ -0,0 +1,131 @@
+﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Aspects.Validation;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+        //IUserService _userService;
+        //IRentalService _rentalService;
+        //public CustomerManager(ICustomerDal customerDal, IUserService userService, IRentalService rentalService)
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+            //_userService = userService;
+            //_rentalService = rentalService;
+        }
+
+        [SecuredOperation("customer.add,customer.admin,admin")]
+        [ValidationAspect(typeof(CustomerValidator))]
+        public IResult Add(Customer customer)
+        {
+            //IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+            //                       CheckIfUserIdExists(customer.UserId),
+            //                       CheckIfCustomerUserIdExists(customer.UserId));
+
+            IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+                                               CheckIfCustomerUserIdExists(customer.UserId));
+            if (result != null)
+            {
+                return result;
+            }
+            _customerDal.Add(customer);
+            return new SuccessResult(Messages.CustomerAdded);
+        }
+
+        [SecuredOperation("customer.delete,customer.admin,admin")]
+        public IResult Delete(Customer customer)
+        {
+            //IResult result = BusinessRules.Run(CheckIfCustomerHasActiveRentalExists(customer.Id));
+            //if (result != null)
+            //{
+            //    return result;
+            //}
+            _customerDal.Delete(customer);
+            return new SuccessResult(Messages.CustomerDeleted);
+        }
+
+        [SecuredOperation("customer.list.getall,customer.admin,admin")]
+        public IDataResult<List<Customer>> GetAll()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(), Messages.CustomersListed);
+        }
+
+        [SecuredOperation("customer.list.getbyid,customer.admin,admin")]
+        public IDataResult<Customer> GetById(int id)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.Get(p => p.Id == id), Messages.CustomerFound);
+        }
+
+        [SecuredOperation("customer.update,customer.admin,admin")]
+        [ValidationAspect(typeof(CustomerValidator))]
+        public IResult Update(Customer customer)
+        {
+            IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+                                               CheckIfCustomerUserIdExists(customer.UserId));
+            if (result != null)
+            {
+                return result;
+            }
+            _customerDal.Update(customer);
+            return new SuccessResult(Messages.CustomerUpdated);
+        }
+        private IResult CheckIfCustomerCompanyNameExists(string CompanyName)
+        {
+            var result = _customerDal.GetAll(p => p.CompanyName == CompanyName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.CustomerCompanyNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCustomerUserIdExists(int UserId)
+        {
+            var result = _customerDal.GetAll(p => p.UserId == UserId).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.CustomerUserIdAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        //private IResult CheckIfUserIdExists(int UserId)
+        //{
+        //    var result = _userService.GetById(UserId);
+
+        //    if (result.Success == false || result.Data == null)
+        //    {
+        //        return new ErrorResult(Messages.UserNotFound);
+        //    }
+        //    return new SuccessResult();
+        //}
+        //private IResult CheckIfCustomerHasActiveRentalExists(int id)
+        //{
+        //    var result = _rentalService.GetRentalsByCustomerId(id);
+
+        //    if (result.Data.Count > 0)
+        //    {
+        //        return new ErrorResult(Messages.CustomerHasActiveRentalExists);
+        //    }
+        //    return new SuccessResult();
+        //}
+        
+        public IDataResult<List<Customer>> GetCustomersByUserId(int id)
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(p => p.UserId == id), Messages.CarsByBrandIdListed);
+        }
+    }
+}
---
+++ KampIntro_Odevler/CarRental/ReCapProject_Gun_15_Odev_01/Business/ValidationRules/FluentValidation/CarValidator.cs
@@ -0,0 +1,28 @@
+﻿using Entities.Concrete;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.ValidationRules.FluentValidation
+{
+    public class CarValidator : AbstractValidator<Car>
+    {
+        public CarValidator()
+        {
+            RuleFor(p => p.BrandId).NotEmpty();
+            RuleFor(p => p.BrandId).GreaterThan(0);
+            RuleFor(p => p.ColorId).NotEmpty();
+            RuleFor(p => p.ColorId).GreaterThan(0);
+            RuleFor(p => p.ModelYear).NotEmpty();
+            RuleFor(p => p.ModelYear).MinimumLength(4);
+            RuleFor(p => p.ModelYear).GreaterThan("2010");
+            RuleFor(p => p.DailyPrice).NotEmpty();
+            RuleFor(p => p.DailyPrice).GreaterThan(0);
+            RuleFor(p => p.Description).NotEmpty();
+            RuleFor(p => p.Description).MinimumLength(2);
+        }
+    }
+}
