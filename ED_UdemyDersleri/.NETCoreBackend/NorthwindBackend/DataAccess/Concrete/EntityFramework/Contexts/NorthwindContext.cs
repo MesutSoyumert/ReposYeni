9785029d265e82ed29a3273f6d4c514745079db7
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-04-05 20:27:22 +0300

    Bölüm 11 workshop ödevi yapıldı, test edildi çalışıyor

--- ED_UdemyDersleri/.NETCoreBackend/NorthwindBackend/DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
+++ ED_UdemyDersleri/.NETCoreBackend/NorthwindBackend/DataAccess/Concrete/EntityFramework/Contexts/NorthwindContext.cs
@@ -15,7 +15,7 @@ namespace DataAccess.Concrete.EntityFramework.Contexts
             optionsBuilder.UseSqlServer(@"Server=(localdb)\MSSQLLocalDB;Database=Northwind;Trusted_Connection=true");
         }
         public DbSet<Product> Products { get; set; }
-        //public DbSet<Category> Categories { get; set; }
+        public DbSet<Category> Categories { get; set; }
         //public DbSet<Customer> Customers { get; set; }
         //public DbSet<Order> Orders { get; set; }
         //public DbSet<User> Users { get; set; }
