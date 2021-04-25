afde54d27516a5bcf5d8e4cf6dcc2703d935faa4
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-04-25 14:52:10 +0300

    Gün 21 günbaşı hazırlık

---
+++ KampIntro/KampIntro_Odevler/CarRental/ReCapProject_Gun_21_Odev_01/Business/Abstract/ICustomerService.cs
@@ -0,0 +1,22 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface ICustomerService
+    {
+        IDataResult<List<Customer>> GetCustomers();
+        IDataResult<Customer> GetById(int id);
+        IDataResult<List<CustomerDetailDto>> GetCustomerDetails();
+        IDataResult<List<CustomerDetailDto>> GetCustomerDetailByUserId(int userId); IResult Add(Customer customer);
+        IResult Update(Customer customer);
+        IResult Delete(Customer customer);
+
+    }
+}
---
+++ KampIntro/KampIntro_Odevler/CarRental/ReCapProject_Gun_21_Odev_01/Business/Concrete/CustomerManager.cs
@@ -0,0 +1,164 @@
+﻿using Business.Abstract;
+using Business.BusinessAspects.Autofac;
+using Business.Constants;
+using Business.ValidationRules.FluentValidation;
+using Core.Aspects.Aspects.Validation;
+using Core.Aspects.Autofac.Caching;
+using Core.CrossCuttingConcerns.Validation;
+using Core.Utilities.Business;
+using Core.Utilities.Results;
+using DataAccess.Abstract;
+using Entities.Concrete;
+using Entities.DTOs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Concrete
+{
+    public class CustomerManager : ICustomerService
+    {
+        ICustomerDal _customerDal;
+        
+        //IUserService _userService;
+        //IRentalService _rentalService;
+        //public CustomerManager(ICustomerDal customerDal, IUserService userService, IRentalService rentalService)
+        public CustomerManager(ICustomerDal customerDal)
+        {
+            _customerDal = customerDal;
+            //_userService = userService;
+            //_rentalService = rentalService;
+        }
+
+        //[SecuredOperation("customer.add,customer.admin,admin")]
+        [ValidationAspect(typeof(CustomerValidator))]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IResult Add(Customer customer)
+        {
+            //IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+            //                       CheckIfUserIdExists(customer.UserId),
+            //                       CheckIfCustomerUserIdExists(customer.UserId));
+
+            IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+                                               CheckIfCustomerUserIdExists(customer.UserId));
+            if (result != null)
+            {
+                return result;
+            }
+            _customerDal.Add(customer);
+            return new SuccessResult(Messages.CustomerAdded);
+        }
+
+        //[SecuredOperation("customer.delete,customer.admin,admin")]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IResult Delete(Customer customer)
+        {
+            //IResult result = BusinessRules.Run(CheckIfCustomerHasActiveRentalExists(customer.Id));
+            //if (result != null)
+            //{
+            //    return result;
+            //}
+            _customerDal.Delete(customer);
+            return new SuccessResult(Messages.CustomerDeleted);
+        }
+
+        //[SecuredOperation("customer.update,customer.admin,admin")]
+        [ValidationAspect(typeof(CustomerValidator))]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IResult Update(Customer customer)
+        {
+            IResult result = BusinessRules.Run(CheckIfCustomerCompanyNameExists(customer.CompanyName),
+                                               CheckIfCustomerUserIdExists(customer.UserId));
+            if (result != null)
+            {
+                return result;
+            }
+            _customerDal.Update(customer);
+            return new SuccessResult(Messages.CustomerUpdated);
+        }
+
+        //[SecuredOperation("customer.list.getall,customer.admin,admin")]
+        [CacheAspect]
+        //[PerformanceAspect(10)]
+        public IDataResult<List<Customer>> GetCustomers()
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll());
+        }
+
+        //[SecuredOperation("customer.list.getbyid,customer.admin,admin")]
+        [CacheAspect]
+        //[PerformanceAspect(10)]
+        public IDataResult<Customer> GetById(int id)
+        {
+            return new SuccessDataResult<Customer>(_customerDal.Get(p => p.Id == id), Messages.CustomerFound);
+        }
+
+        //[SecuredOperation("customer.list.getcustomersbyuserid,customer.admin,admin")]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IDataResult<List<Customer>> GetCustomersByUserId(int id)
+        {
+            return new SuccessDataResult<List<Customer>>(_customerDal.GetAll(p => p.UserId == id), Messages.CarsByBrandIdListed);
+        }
+
+        //[SecuredOperation("customer.list.getcustomersbyuserid,customer.admin,admin")]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IDataResult<List<CustomerDetailDto>> GetCustomerDetails()
+        {
+            return new SuccessDataResult<List<CustomerDetailDto>>(_customerDal.GetCustomerDetailDto());
+        }
+
+        //[SecuredOperation("customer.list.getcustomersbyuserid,customer.admin,admin")]
+        [CacheRemoveAspect("ICustomerService.Get")]
+        //[PerformanceAspect(10)]
+        public IDataResult<List<CustomerDetailDto>> GetCustomerDetailByUserId(int userId)
+        {
+            return new SuccessDataResult<List<CustomerDetailDto>>(_customerDal.GetCustomerDetailDto(u => u.UserId == userId));
+        }
+        
+        private IResult CheckIfCustomerCompanyNameExists(string CompanyName)
+        {
+            var result = _customerDal.GetAll(p => p.CompanyName == CompanyName).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.CustomerCompanyNameAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        private IResult CheckIfCustomerUserIdExists(int UserId)
+        {
+            var result = _customerDal.GetAll(p => p.UserId == UserId).Any();
+            if (result)
+            {
+                return new ErrorResult(Messages.CustomerUserIdAlreadyExists);
+            }
+            return new SuccessResult();
+        }
+        //private IResult CheckIfUserIdExists(int UserId)
+        //{
+        //    var result = _userService.GetById(UserId);
+
+        //    if (result.Success == false || result.Data == null)
+        //    {
+        //        return new ErrorResult(Messages.UserNotFound);
+        //    }
+        //    return new SuccessResult();
+        //}
+        //private IResult CheckIfCustomerHasActiveRentalExists(int id)
+        //{
+        //    var result = _rentalService.GetRentalsByCustomerId(id);
+
+        //    if (result.Data.Count > 0)
+        //    {
+        //        return new ErrorResult(Messages.CustomerHasActiveRentalExists);
+        //    }
+        //    return new SuccessResult();
+        //}
+    }
+}
