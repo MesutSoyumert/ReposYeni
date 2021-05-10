be9111fde857050ecc89d05cf12c70512e11b1a7
Mesut SOYUMERT <msoyumert@hotmail.com>
2021-05-10 14:51:14 +0300

    Gün 06 Ödev 03 bitti

---
+++ Java_React_Ödevler/Java_React_Ödev_Gün_06_Ödev_03_hrms/hrms/hrms/src/main/java/soyumert/hrms/business/abstracts/SystemPersonelService.java
@@ -0,0 +1,11 @@
+package soyumert.hrms.business.abstracts;
+
+import java.util.List;
+
+import soyumert.hrms.entities.concretes.SystemPersonel;
+
+public interface SystemPersonelService {
+	
+	List<SystemPersonel> getAll();
+
+}
---
+++ Java_React_Ödevler/Java_React_Ödev_Gün_06_Ödev_03_hrms/hrms/hrms/src/main/java/soyumert/hrms/entities/concretes/SystemPersonel.java
@@ -0,0 +1,41 @@
+package soyumert.hrms.entities.concretes;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name="systemPersonel")
+
+public class SystemPersonel {
+
+	@Id
+	@GeneratedValue
+	@Column(name="id")
+	private int id;
+	
+	@Column(name="first_name")
+	private String firstName;
+	
+	@Column(name="last_name")
+	private String lastName;
+	
+	@Column(name="email")
+	private String email;
+	
+	//public SystemPersonel() {}
+	
+	public SystemPersonel(int id, String firstName, String lastName, String email) {
+		super();
+		this.id = id;
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+}
